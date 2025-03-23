7923de43990bb8e491fab1721d5cefed1d66f694
fduxiao <9294979+fduxiao@users.noreply.github.com>
2025-03-23 05:56:41 -0400

    add frontend

---
+++ rag_db_front/src/views/HomeView.vue
@@ -0,0 +1,14 @@
+<script setup>
+import TheWelcome from '../components/TheWelcome.vue'
+</script>
+
+<template>
+  <main>
+    <el-button>Default</el-button>
+    <el-button type="primary">Primary</el-button>
+    <el-button type="success">Success</el-button>
+    <el-button type="info">Info</el-button>
+    <el-button type="warning">Warning</el-button>
+    <el-button type="danger">Danger</el-button>
+  </main>
+</template>
